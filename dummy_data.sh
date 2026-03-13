990235bed9030cb95f5f48605d469fe6f9dcb6de
Pacheco,Christopher D <CDPacheco@mdanderson.org>
2026-03-12 19:31:15 -0500

    Update dummy_data.sh

--- dummy_data.sh
+++ dummy_data.sh
@@ -4,26 +4,14 @@
 # File data is from '/dev/zero' so all files with the same size will have the same hash
 # Enable RANDOM_DATA to write random data from '/dev/urandom'
 
-RANDOM_DATA=0              # Set to 1 to enable random data
-BLOCK_SIZE='1MB'
-BLOCK_COUNT=100
-DATA_SRC='/dev/zero'
-
-
-if [[ RANDOM_DATA -eq 1 ]]; then DATA_SRC='/dev/urandom'
-echo "Input data is from: ${DATA_SRC}"
-
-# HELP ARGUMENTS
+# HELP TEXT
 if [[ "$1" == "-h" || "$1" == "--help" ]]; then
     echo "This script creates a dummy file with a random name for testing purposes"
     echo "The file size is equal to BLOCK_COUNT in Megabytes"
-    echo "example: $> ./dummy_data.sh [BLOCK_COUNT]"
-    echo "   
+    echo "example:" 
+	echo "./dummy_data.sh [BLOCK_COUNT]"
     exit 0
 fi
-
-if [[ - "$1" ]]
-
 
 generate_name() {
     # Returns a unique file name in the format `file_#.dat` starting at 1
@@ -39,11 +27,26 @@ generate_name() {
     while [[ -f "${BASE}${NUM}${EXT}" ]]; do
         NUM+=1
     done
-
+    
+	#Return file name
     local OUT_FILE="${BASE}${NUM}${EXT}"
     echo "$OUT_FILE"
 }
 
+# ---- START MAIN SCRIPT ----
+
+RANDOM_DATA=0              # Set to 1 to enable random data
+BLOCK_SIZE='1MB'
+BLOCK_COUNT=100
+DATA_SRC='/dev/zero'
+
+if [[ RANDOM_DATA -eq 1 ]]; then DATA_SRC='/dev/urandom'; fi
+echo "Input data is from: ${DATA_SRC}"
+
+# Check for positional variables 
+if [[ -n "$1" ]]; then BLOCK_COUNT="$1"; fi
+
+# Generate file name
 OUT_FILE=$(generate_name)
 
 echo "dd if=$DATA_SRC of=$OUT_FILE bs=$BLOCK_SIZE count=$BLOCK_COUNT"
