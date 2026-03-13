b39f6b2dc83329023457514478cc215f886af34c
Pacheco,Christopher D <CDPacheco@mdanderson.org>
2026-03-13 18:17:46 -0500

    Update hasher.py
    created xxhash64 function
    TODO: needs to be tested for consistency and optimize the file loading

--- hasher.py
+++ hasher.py
@@ -1,13 +1,33 @@
 from pathlib import Path
 import xxhash
 
-file = Path('test1.dat')
+"""
+The xxhash library has multiple options for hashing algorithms
+xxhash32
+xxhash64
+xxh3_64
+xxh3_128
+"""
 
-algo = xxhash.xxh128()
-# outputs a file hash
-with open(file, mode='rb') as f:
-    chunk = f.read()
-    algo.update(chunk)
-    out = algo.hexdigest()
+#TODO make sure large files are read in a byte stream
+#TODO confirm the correct hash function is being used
+#TODO confirm consistency of hashes compared to the linux xxhash library 
+# when using `xxhsum -H3 ./file` command
+def xxhash64(file) -> str:
+    hash_factory = xxhash.xxh3_64()
+    with open(file=file, mode='rb',) as f:
+        while True:
+            chunk = f.read()
+            if chunk == b'':
+                break
+            hash_factory.update(chunk)
+            #chunk = f.read()
+        file_hash = hash_factory.hexdigest()
+        hash_factory.reset()
+        return file_hash
 
-print(out)
+if __name__ == '__main__':
+    _hash1 = xxhash64(file=Path('./file_2.dat'))
+    print(str(_hash1))
+    
+    # _hash2 = xxhash.xxh3_64_hexdigest(Path('./file_2.dat'))
