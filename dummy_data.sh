b7207f1f1ef3cb8a0d9de06b9312953b9dd9f120
Pacheco,Christopher D <CDPacheco@mdanderson.org>
2026-03-13 15:26:56 -0500

    Update dummy_data.sh
    Update: argument parsing to include option handling logic
    New: random data generation option -r
    Fix: file name no longer misses underscore "_"
    Update: Default block count changed (100 -> 10)

--- dummy_data.sh
+++ dummy_data.sh
@@ -4,6 +4,12 @@
 # File data is from '/dev/zero' so all files with the same size will have the same hash
 # Enable RANDOM_DATA to write random data from '/dev/urandom'
 
+# Initialize defaults
+RANDOM_DATA=0              # Set to 1 to enable random data
+BLOCK_SIZE='1MB'
+BLOCK_COUNT=10
+DATA_SRC='/dev/zero'
+
 # HELP TEXT
 if [[ "$1" == "-h" || "$1" == "--help" ]]; then
     echo "This script creates a dummy file with a random name for testing purposes"
@@ -13,13 +19,30 @@ if [[ "$1" == "-h" || "$1" == "--help" ]]; then
     exit 0
 fi
 
+# handle arguments
+while [[ $# -gt 0 ]]; do
+    case "$1" in
+        -r)
+            # enable random data
+            RANDOM_DATA=1
+            shift 1
+            ;;
+        *)
+            # handle positional args
+            if [[ -n "$1" ]]; then BLOCK_COUNT="$1"; fi
+            shift 1
+            ;;
+    esac
+done
+
+# Define Functions
 generate_name() {
     # Returns a unique file name in the format `file_#.dat` starting at 1
     # (eg. file_1.dat, file_2.dat, ..., file_N.dat)
 
     # initialize variables
     declare -i NUM
-    BASE='file'
+    BASE='file_'
     NUM=1
     EXT='.dat'
 
@@ -35,18 +58,12 @@ generate_name() {
 
 # ---- START MAIN SCRIPT ----
 
-RANDOM_DATA=0              # Set to 1 to enable random data
-BLOCK_SIZE='1MB'
-BLOCK_COUNT=100
-DATA_SRC='/dev/zero'
-
-if [[ RANDOM_DATA -eq 1 ]]; then DATA_SRC='/dev/urandom'; fi
+if [[ RANDOM_DATA -eq 1 ]]; then 
+    DATA_SRC='/dev/urandom'; fi
 echo "Input data is from: ${DATA_SRC}"
 
-# Check for positional variables 
-if [[ -n "$1" ]]; then BLOCK_COUNT="$1"; fi
-
 # Generate file name
-OUT_FILE=$(generate_name)
+OUT_FILE="$(generate_name)"
+echo "output file: ${OUT_FILE}"
+dd if=$DATA_SRC of=$OUT_FILE bs=$BLOCK_SIZE count=$BLOCK_COUNT
 
-echo "dd if=$DATA_SRC of=$OUT_FILE bs=$BLOCK_SIZE count=$BLOCK_COUNT"
