c2e5b310d323d04acf0e704c62e06f3788be3edc
Pacheco,Christopher D <CDPacheco@mdanderson.org>
2026-03-16 18:17:50 -0500

    Update file_scan.py

--- file_scan.py
+++ file_scan.py
@@ -4,6 +4,7 @@ from hasher import xxhash64
 from pprint import pprint
 from dataclasses import dataclass
 
+
 root=r'F:\_dummy_data\recursive_file_tree' 
 root=Path(root)
 
@@ -12,7 +13,7 @@ root=Path(root)
 #     _path: pathlib.Path
 #     depth: int
 
-
+pathlib.PurePosixPath
 
 
 
@@ -32,14 +33,15 @@ for path, _dirs, _files in directory_tree:
             _hash = xxhash64(file)
         file_list.append((_hash, file.__str__()))
 
+def walk_directory_tree
 
 
-hash_set = \
-    {_hash for _hash, _ in file_list}
-conType = type(hash_set)    
-print(len(hash_set))
+# hash_set = \
+#     {_hash for _hash, _ in file_list}
+# conType = type(hash_set)    
+# print(len(hash_set))
 
-pprint(
-    {_hash for _hash, _ in file_list},
-    width=200
-)
+# pprint(
+#     {_hash for _hash, _ in file_list},
+#     width=200
+# )
