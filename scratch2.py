f48adc8a1536d109866101995a7cad0fcaa130d6
Pacheco,Christopher D <CDPacheco@mdanderson.org>
2026-03-16 18:17:55 -0500

    Update scratch2.py

--- scratch2.py
+++ scratch2.py
@@ -7,12 +7,10 @@ fpath = Path('dir_tree.txt')
 with open(fpath, mode='r') as f:
     data: list[Path] = [Path(dat) for dat in f.readlines()]
     
-@dataclass
-class File:
-    path: pathlib.Path
-    root: pathlib.Path
 
-Files = [File(dat, dat.root) for dat in data]
 
-print(Files)
-
+def read_file_list(fpath:Path):
+    with open(file=fpath,mode='r') as handle:
+        lines = [str.strip(line) for line in handle.readlines()]
+    return lines
+        
