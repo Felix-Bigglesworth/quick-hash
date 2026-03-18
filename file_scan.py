59b443387118b23b7be6fd72f240c6e6bb1a4fd7
Pacheco,Christopher D <CDPacheco@mdanderson.org>
2026-03-18 18:09:42 -0500

    Update file_scan.py
    added function walk_directory
    started implementing a container to group all the info and functions

--- file_scan.py
+++ file_scan.py
@@ -1,47 +1,36 @@
 from pathlib import Path
+from os import PathLike
+import os
+import sys
 import pathlib
+from pprint import pprint as pp
 from hasher import xxhash64
 from pprint import pprint
 from dataclasses import dataclass
 
+PLATFORM = sys.platform # Windows: 'win32'; Linux: 'linux'
+drive = Path(os.getcwd()).resolve()
+print(drive)
 
-root=r'F:\_dummy_data\recursive_file_tree' 
-root=Path(root)
+# Adds the to_str method to pathlib.Path objects
+# replace with `Path.__str__()` ?
+def to_str(self):
+    return str(self)
+Path.to_str = to_str
 
-# @dataclass()
-# class Directory:
-#     _path: pathlib.Path
-#     depth: int
-
-pathlib.PurePosixPath
-
-
-
-
-
-# Get a full file list
-# outputs a legth=3 tuple with the following:
-
-file_list = []
-
-# add all files to a list while skipping directories
-directory_tree = root.walk()
-for path, _dirs, _files in directory_tree:
-    for f in _files:
-        file = path / f
-        if file.exists():
-            _hash = xxhash64(file)
-        file_list.append((_hash, file.__str__()))
-
-def walk_directory_tree
+# Walking the filesystem can be done using either pathlib.Path.walk() or pathlib.path.rglob()
+def walk_directory(start:Path) -> os.Iterator[Path]:
+    # recursively search the given directory for all subdirectories and files.
+    # Note: includes the starting directory itself
+    return start.rglob(pattern='**')
 
 
-# hash_set = \
-#     {_hash for _hash, _ in file_list}
-# conType = type(hash_set)    
-# print(len(hash_set))
+# Rename and inherit from pathlib.Path??
+class File:
+    def __init__(self, path: Path):
+        self.path = path
+        self.is_dir = path.is_dir()
+        self.is_file = path.is_file()
+        
+    
 
-# pprint(
-#     {_hash for _hash, _ in file_list},
-#     width=200
-# )
