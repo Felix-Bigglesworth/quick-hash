6ed1fcdbfce7a0b0590edf43e06f3ac392624985
CDP <123410106+Felix-Bigglesworth@users.noreply.github.com>
2026-03-22 19:06:00 -0500

    Rename file_utils to utils

--- utils.py
+++ utils.py
@@ -4,7 +4,6 @@ import pathlib
 from dataclasses import dataclass
 
 # WRITE FILES
-
 def append_newlines_list(lines: list[Path]):
     # append a \n character to each list entry
     return [str(f) + '\n' for f in lines]
@@ -22,7 +21,6 @@ def write_tree_to_file(root_dir:Path, outfile:Path):
 
 
 # READ FILES
-
 def load_file_list(fpath:Path, os_type=None):
     with open(file=fpath,mode='r') as handle:
         lines = [str_to_path(line, _type=os_type) for line in handle.readlines()]
@@ -49,8 +47,7 @@ def str_to_path(string:str, _type):
 #     _drive, _root = (pure_path.drive, pure_path.root)
 #     return ''.join([_drive, _root])
 
-def file_depth(file_path: pathlib.PurePath):
-    file_path.parts()
+
     
 
 # ==== FILE DATA ====
