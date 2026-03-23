ef95a8efaed91b854c59d26cd59ac92509e47348
CDP <123410106+Felix-Bigglesworth@users.noreply.github.com>
2026-03-22 19:07:56 -0500

    Updated file_scan.py
    Added new class ExtendedPath
    Included hashing function
    Began introducing logs

--- file_scan.py
+++ file_scan.py
@@ -1,36 +1,115 @@
-from pathlib import Path
-from os import PathLike
-import os
-import sys
+import logging
 import pathlib
-from pprint import pprint as pp
+from pathlib import Path
+import os
+from os import PathLike
+import sys
 from hasher import xxhash64
-from pprint import pprint
+from pprint import pprint as pp
 from dataclasses import dataclass
+import xxhash
+import time
+from constants import CHUNK_SIZE
 
-PLATFORM = sys.platform # Windows: 'win32'; Linux: 'linux'
-drive = Path(os.getcwd()).resolve()
-print(drive)
+logging.basicConfig(
+    level=logging.DEBUG,                          # Minimum level to capture
+    format="%(asctime)s | %(levelname)-8s | %(message)s",
+    datefmt="%Y-%m-%d %H:%M:%S",
+    handlers=[
+        logging.StreamHandler(),                  # Print to console
+        logging.FileHandler("my_script.log"),     # Also write to file
+    ]
+)
+log = logging.getLogger(__name__)
 
-# Adds the to_str method to pathlib.Path objects
-# replace with `Path.__str__()` ?
-def to_str(self):
-    return str(self)
-Path.to_str = to_str
+# PLATFORM = sys.platform # SYSTEM_OS: 'win32' or 'linux'
+log.info('Platform=%s | Script: %s', __file__, sys.platform)
+
+
+
 
 # Walking the filesystem can be done using either pathlib.Path.walk() or pathlib.path.rglob()
-def walk_directory(start:Path) -> os.Iterator[Path]:
+def walk_directory(start:Path):
     # recursively search the given directory for all subdirectories and files.
     # Note: includes the starting directory itself
     return start.rglob(pattern='**')
 
+def xxh3(file: Path) -> str:
+    hasher = xxhash.xxh3_64()
+    with open(file=file, mode='rb',) as f:
+        while chunk := (f.read(CHUNK_SIZE)):
+            hasher.update(chunk)
+        file_hash = hasher.hexdigest()
+        hasher.reset()
+        return file_hash
 
-# Rename and inherit from pathlib.Path??
-class File:
-    def __init__(self, path: Path):
-        self.path = path
-        self.is_dir = path.is_dir()
-        self.is_file = path.is_file()
+# Extend pathlib.Path functionality
+class ExtendedPath(Path):
+    def __new__(cls, *args):
+        return super().__new__(cls, *args)
+
+    def __init__(self, *args):
+        super().__init__(*args)
+        _stat = self.stat()
+        self.size = _stat.st_size
+        self.modified = _stat.st_mtime
+        self.created = _stat.st_birthtime \
+            if hasattr(self, 'st_birthtime') \
+            else _stat.st_ctime # Accounts for weird windows quirk where birthtime is used
+        # self._created = _stat.st_ctime
+        self.hard_links = _stat.st_nlink 
+        
+
+    
+    @property    
+    def depth(self) -> int:
+        '''
+        Equal to the depth below the root directory (root depth=0)
+        
+        Examples:
+        "/etc/hosts" -> depth=2
+        "/home/username/images/sample1.tiff" -> depth=4
+        '''
+        return len(self.parts)
+    
+    @property
+    def file_type(self):
+        return ''.join(self.suffixes)
+    
+    def as_string(self):
+        return str(self)
+    
+    def file_hash(self) -> str:
+        outhash = xxh3(self)
+        return outhash
+    
+    def summary(self):
+        _str = self.as_string()
+        _hash = self.file_hash()
+        _depth = self.depth
+        _created = self.created
+        _modified = self.modified
+        
+        f1 = f'file name: {_str}'
+        f2 = f'file depth: {_depth}' 
+        f3 = f'file hash: {_hash}'
+        f4 = f'created: {_created}'
+        f5 = f'modified: {_modified}'
+        
+        print(f1, f2, f3, f4, f5, sep='\n')
+            
         
     
 
+if __name__ == '__main__':
+    ex = ExtendedPath(
+        '/home/cdpacheco/Documents/GitHub'
+        )
+    print(
+    ex.created
+    )
+    print(
+    time.localtime(ex.modified)
+    )
+    
+    
