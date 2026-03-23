d5090585c7dce24e444a1f2e9252f3999551a6a7
Pacheco,Christopher D <CDPacheco@mdanderson.org>
2026-03-23 17:41:17 -0500

    Update file_scan.py
    Added logging to file_scan.py

--- file_scan.py
+++ file_scan.py
@@ -1,6 +1,7 @@
+import itertools
 import logging
 import pathlib
-from pathlib import Path
+from pathlib import Path, PurePosixPath, PureWindowsPath
 import os
 from os import PathLike
 import sys
@@ -34,7 +35,7 @@ def walk_directory(start:Path):
     # Note: includes the starting directory itself
     return start.rglob(pattern='**')
 
-def xxh3(file: Path) -> str:
+def xxh3_64(file: Path) -> str:
     hasher = xxhash.xxh3_64()
     with open(file=file, mode='rb',) as f:
         while chunk := (f.read(CHUNK_SIZE)):
@@ -50,6 +51,7 @@ class ExtendedPath(Path):
 
     def __init__(self, *args):
         super().__init__(*args)
+        log.debug('initializeing extendedpath: windows=%s posix=%s', isinstance(self, PureWindowsPath), isinstance(self, PurePosixPath))
         _stat = self.stat()
         self.size = _stat.st_size
         self.modified = _stat.st_mtime
@@ -57,7 +59,18 @@ class ExtendedPath(Path):
             if hasattr(self, 'st_birthtime') \
             else _stat.st_ctime # Accounts for weird windows quirk where birthtime is used
         # self._created = _stat.st_ctime
-        self.hard_links = _stat.st_nlink 
+        self.hard_links = _stat.st_nlink
+        
+        _attributes = {
+            self.size
+        }
+        logging.debug('size=%(size)d | modified=%(modified)d | created=%(created)d | hard_links=%(hard_links)i',
+                         {
+                          'size':self.size, 
+                          'modified':self.modified, 
+                          'created':self.created, 
+                          'hard_links':self.hard_links,
+                          })
         
 
     
@@ -66,22 +79,42 @@ class ExtendedPath(Path):
         '''
         Equal to the depth below the root directory (root depth=0)
         
-        Examples:
-        "/etc/hosts" -> depth=2
+        Examples:   
+        "/etc/hosts" -> depth=2  
         "/home/username/images/sample1.tiff" -> depth=4
         '''
         return len(self.parts)
     
     @property
     def file_type(self):
+        log.debug('method: file_type | file: %s | suffix: %s', self.name, self.suffixes)
         return ''.join(self.suffixes)
     
     def as_string(self):
         return str(self)
     
     def file_hash(self) -> str:
-        outhash = xxh3(self)
-        return outhash
+        log.debug('hashing %s', str(self))
+
+        hasher = xxhash.xxh3_64()
+        
+        try:
+            with open(file=self, mode='rb',) as f:
+                chunk_count = itertools.count()
+                while chunk := (f.read(CHUNK_SIZE)):
+                    next(chunk_count)
+                    hasher.update(chunk)
+                    log.debug('chunks_processed: %i', chunk_count)
+            file_hash = hasher.hexdigest()
+            log.debug('Chunks processed: %i | Hash: %s \nfile: %s', chunk_count, str(file_hash), str(self))
+        except FileNotFoundError:
+            log.warning('file %s not found', self.name)
+            file_hash = ''
+        finally:
+            hasher.reset()
+            log.debug('resetting hasher')
+        
+        return str(file_hash)
     
     def summary(self):
         _str = self.as_string()
